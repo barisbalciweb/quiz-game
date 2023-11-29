ec08b89ee31735956c9e04d3f4d84678d382168a
Barış Balcı <126829019+barisbalcimusic@users.noreply.github.com>
2023-11-30 00:35:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+fetchData();
+
+let answer;
+
+async function fetchData() {
+  try {
+    let response = await fetch("./questions.json");
+    let data = await response.json();
+
+    let randomNr = Math.floor(Math.random() * data.length);
+
+    let question = data[randomNr].Frage.replace(/</g, "&lt;").replace(
+      />/g,
+      "&gt;"
+    );
+
+    answer = data[randomNr].Antwort.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+    let optionA = data[randomNr].A.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    let optionB = data[randomNr].B.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    let optionC = data[randomNr].C.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    let optionD = data[randomNr].D.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+    document.getElementById("question").innerHTML = question;
+    document.getElementById("A-label").innerHTML = "A) " + optionA;
+    document.getElementById("B-label").innerHTML = "B) " + optionB;
+    document.getElementById("C-label").innerHTML = "C) " + optionC;
+    document.getElementById("D-label").innerHTML = "D) " + optionD;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+document.getElementById("A").addEventListener("click", function () {
+  answerCheck("A");
+});
+document.getElementById("B").addEventListener("click", function () {
+  answerCheck("B");
+});
+document.getElementById("C").addEventListener("click", function () {
+  answerCheck("C");
+});
+document.getElementById("D").addEventListener("click", function () {
+  answerCheck("D");
+});
+
+var isAnswerSelected = false;
+
+function answerCheck(par) {
+  if (isAnswerSelected == true) {
+    return;
+  }
+  let label = document.getElementById(par + "-label");
+  if (answer == par) {
+    label.style.backgroundColor = "green";
+    isAnswerSelected = true;
+  } else {
+    label.style.backgroundColor = "red";
+    label = document.getElementById(answer + "-label");
+    label.style.backgroundColor = "green";
+    isAnswerSelected = true;
+  }
+  setTimeout(reload, 3000);
+
+  function reload() {
+    location.reload();
+  }
+}
+
+// DOM Manipulation ya da tag icinde script kullanarak objeden ilgili nesleneri cagir
+// bir sonraki soruya gecmesi icin ayni varible ye kosuldan sonra baska bir veri tanimla.
+// bunu da döngü ile yap.
+// dogru veya yanlis cevapta settimeintervall kullanabilirsin yaniü sönme efekti icin
+// dogru ve yanlis cevap sayisina göre bir sayac hazirla ve onu her seferinde güncelle ve
+// oyun sonunda skor olarak göster
